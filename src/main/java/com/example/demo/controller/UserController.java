23cc1b23333b6deec84b824abf865f7c8cf2a614
李维俊 <weijun.li@supwisdom.com>
2019-05-09 17:45:14 +0800

    支持Swagger2

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,17 +20,22 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @ApiOperation(value="获取用户列表", notes="")
+
     @RequestMapping(value = "/{id}",method = RequestMethod.GET)
     public User getUserList(@PathVariable("id") Integer id){
         User user = userService.getUserById(id);
         return user;
     }
 
+    @ApiOperation(value = "根据id删除用户", notes = "")
     @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
     public void deleteById(@PathVariable("id") Integer id){
         userService.deleteById(id);
     }
 
+    @ApiOperation(value = "创建新用户", notes = "")
+    @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
     @RequestMapping(value = "/user",method = RequestMethod.POST)
     public int createUser(@RequestBody User user){
         return userService.createUser(user);
